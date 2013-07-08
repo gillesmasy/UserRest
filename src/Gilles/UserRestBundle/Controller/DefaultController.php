8e9355fe90e2c3ec0222b5d22b01bc396eba972a
gillesmasy <jlou42169@gmail.com>
2013-07-08 15:41:19 +0200

    Corrections

--- src/Gilles/UserRestBundle/Controller/DefaultController.php
+++ src/Gilles/UserRestBundle/Controller/DefaultController.php
@@ -26,39 +26,50 @@ class DefaultController extends Controller
     public function CreateAction(){
         $response = new Response();
         
-        $content = $this->get('request')->getContent();
-        if(! $content){
-            //Retour BAD REQUEST - sans texte
-            return $response
-                    ->setStatusCode(400)
-                    ->setContent(json_encode("Empty value"));
+        try{
+            $content = $this->get('request')->getContent();
+            if(! $content){
+                //Retour BAD REQUEST - sans texte
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode("Empty value"));
+            }
+            $data = json_decode($content, true);
+            
+            if($data)
+                $user = new Utilisateur($data, $this->get('security.encoder_factory'));
+            else
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode("Empty data"));
+            
+            $validator = $this->get('validator');
+            $errorList = $validator->validate($user);
+
+            //L'utilisateur est validé
+            if(count($errorList) <= 0){
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                //Retour OK
+                return $response
+                        ->setStatusCode(200)
+                        ->setContent($user->getId());
+            }else{
+                $msgError = array();
+                foreach($errorList as $error)
+                    $msgError[$error->getPropertyPath()]= $error->getMessage();
+
+                //Retour BAD REQUEST - avec les messages d'erreurs
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode($msgError));
+            }
         }
-        $data = json_decode($content, true);
-        
-        $user = new Utilisateur($data, $this->get('security.encoder_factory'));
-        
-        $validator = $this->get('validator');
-        $errorList = $validator->validate($user);
-        
-        //L'utilisateur est validé
-        if(count($errorList) <= 0){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
-            
-            //Retour OK
+        catch(\Exception $e){
             return $response
-                    ->setStatusCode(200)
-                    ->setContent($user->getId());
-        }else{
-            $msgError = array();
-            foreach($errorList as $error)
-                $msgError[$error->getPropertyPath()]= $error->getMessage();
-            
-            //Retour BAD REQUEST - avec les messages d'erreurs
-            return $response
-                    ->setStatusCode(400)
-                    ->setContent(json_encode($msgError));
+                    ->setStatusCode(500);
         }
     }
     
@@ -68,20 +79,28 @@ class DefaultController extends Controller
     public function GetAction($id){
         $response = new Response();
         
-        if(! $id){
-            return $response->setStatusCode(400);
+        try{
+            if(! $id){
+                return $response->setStatusCode(400);
+            }
+
+            $user = $this->getDoctrine()
+                    ->getRepository('GillesUserRestBundle:Utilisateur')
+                    ->find($id);        
+
+            if(! $user){
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode(array('error' => 'Id Unknown')));
+            }
+
+            return $response
+                    ->setContent($user->toJson());
         }
-        
-        $user = $this->getDoctrine()
-                ->getRepository('GillesUserRestBundle:Utilisateur')
-                ->find($id);        
-        
-        if(! $user){
-            return $response->setStatusCode(400);
+        catch(\Exception $e){
+            return $response
+                    ->setStatusCode(500);
         }
-        
-        return $response
-                ->setContent($user->toJson());
     }
     
     /*
@@ -90,57 +109,63 @@ class DefaultController extends Controller
     public function UpdateAction($id){
         $response = new Response();
         
-        if(! $id){
-            return $response
-                    ->setStatusCode(400)
-                    ->setContent(json_encode(array('error' => 'Id User Unknown')));
+        try{
+            if(! $id){
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode(array('error' => 'Id User Unknown')));
+            }
+
+            $user = $this->getDoctrine()
+                    ->getRepository('GillesUserRestBundle:Utilisateur')
+                    ->find($id);
+            if(! $user){
+                return $response->setStatusCode(400)
+                        ->setContent(json_encode(array('error' => 'User Unknown')));
+            }
+
+            $content = $this->get('request')->getContent();
+            if(! $content){
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode(array('error' => 'No update')));
+            }
+
+            //Mettre à jour l'utilisateur
+            $data = json_decode($content, true);
+            if($data)
+                $user->update($data);
+            else
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode(array('error' => 'No update')));
+
+            $validator = $this->get('validator');
+            $errorList = $validator->validate($user);
+
+            //L'utilisateur est validé
+            if(count($errorList) <= 0){
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                //Retour OK
+                return $response
+                        ->setStatusCode(200);
+            }else{
+                $msgError = array();
+                foreach($errorList as $error)
+                    $msgError[$error->getPropertyPath()]= $error->getMessage();
+
+                //Retour BAD REQUEST - avec les messages d'erreurs
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode($msgError));
+            }
         }
-        
-        $user = $this->getDoctrine()
-                ->getRepository('GillesUserRestBundle:Utilisateur')
-                ->find($id);
-        if(! $user){
-            return $response->setStatusCode(400)
-                    ->setContent(json_encode(array('error' => 'User Unknown')));
-        }
-        
-        $content = $this->get('request')->getContent();
-        if(! $content){
+        catch(\Exception $e){
             return $response
-                    ->setStatusCode(400)
-                    ->setContent(json_encode(array('error' => 'No update')));
-        }
-        
-        //Mettre à jour l'utilisateur
-        $data = json_decode($content, true);
-        if($data)
-            $user->update($data);
-        else
-            return $response
-                    ->setStatusCode(400)
-                    ->setContent(json_encode(array('error' => 'No update')));
-            
-        $validator = $this->get('validator');
-        $errorList = $validator->validate($user);
-        
-        //L'utilisateur est validé
-        if(count($errorList) <= 0){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
-            
-            //Retour OK
-            return $response
-                    ->setStatusCode(200);
-        }else{
-            $msgError = array();
-            foreach($errorList as $error)
-                $msgError[$error->getPropertyPath()]= $error->getMessage();
-            
-            //Retour BAD REQUEST - avec les messages d'erreurs
-            return $response
-                    ->setStatusCode(400)
-                    ->setContent(json_encode($msgError));
+                    ->setStatusCode(500);
         }
     }
     
@@ -150,24 +175,30 @@ class DefaultController extends Controller
     public function DeleteAction($id){
         $response = new Response();
         
-        if(! $id){
-            //Retour BAD REQUEST - sans texte
+        try{
+            if(! $id){
+                //Retour BAD REQUEST - sans texte
+                return $response
+                        ->setStatusCode(400)
+                        ->setContent(json_encode(array('error' => 'No user to delete')));
+            }
+
+            $user = $this->getDoctrine()
+                    ->getRepository('GillesUserRestBundle:Utilisateur')
+                    ->find($id);
+
+            if($user){
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($user);
+                $em->flush();
+            }
+            //Si l'utilisateur n'est pas connu, on considère qu'il est déjà supprimé => ce n'est pas un cas d'erreur
+
+            return $response->setStatusCode(200);
+        }
+        catch(\Exception $e){
             return $response
-                    ->setStatusCode(400)
-                    ->setContent(json_encode(array('error' => 'No user to delete')));
+                    ->setStatusCode(500);
         }
-        
-        $user = $this->getDoctrine()
-                ->getRepository('GillesUserRestBundle:Utilisateur')
-                ->find($id);
-        
-        if($user){
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($user);
-            $em->flush();
-        }
-        //Si l'utilisateur n'est pas connu, on considère qu'il est déjà supprimé => ce n'est pas un cas d'erreur
-        
-        return $response->setStatusCode(200);
     }
 }
